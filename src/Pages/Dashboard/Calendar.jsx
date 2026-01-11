8e7830b3124c1b79c8a28eef59867192248e4c0b
ejazahmad18 <numen.ejaz@gmail.com>
2026-01-11 17:19:09 +0530

    DashBoard-Functionality-Clender

---
+++ src/Pages/Dashboard/Calendar.jsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+const DashboardCalendar = () => {
+  const [date, setDate] = useState(new Date());
+
+  return (
+    <div className="bg-white rounded-xl p-5 shadow">
+      <h3 className="font-semibold text-gray-700 mb-3">
+        Calendar
+      </h3>
+
+      <Calendar
+        onChange={setDate}
+        value={date}
+        className="rounded-lg border-none"
+      />
+    </div>
+  );
+};
+
+export default DashboardCalendar;
