01f8d273bf0c622fdd98c497941ec6a77fe626f7
ejazahmad18 <numen.ejaz@gmail.com>
2026-01-11 18:15:45 +0530

    Create-Hooks-Theme

---
+++ src/Hooks/useTheme.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+const useTheme = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove("light", "dark");
+    root.classList.add(theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return { theme, setTheme };
+};
+
+export default useTheme;
